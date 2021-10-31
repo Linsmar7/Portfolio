b1aa2355c85e73af5a8b9f7ce50bcde57464892e
Linsmar <linsmarvital@gmail.com>
2021-10-31 23:42:30 +0100

    cica linda

--- meusite/pages/index.js
+++ meusite/pages/index.js
@@ -4,8 +4,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <>
-      <p className='text-purple-700 text-opacity-100'>Namora cmg jeni?</p>
-      <p className='text-purple-700 text-opacity-0'>Se a resposta for n, namora cmg ciça?</p>
+      <p className='text-purple-700 text-opacity-100'>Ciça mto linda n tem como</p>
     </>
   )
 }
