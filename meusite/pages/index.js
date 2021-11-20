d9cda6422efd2e4ebc38e05b729025d651b4b9ab
Linsmar <linsmarvital@gmail.com>
2021-11-20 17:06:08 +0000

    Troll

--- meusite/pages/index.js
+++ meusite/pages/index.js
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <>
       <p className='text-purple-700 text-opacity-100'>Ciça mto linda n tem como</p>
+      <h1>SITE EM CONSTRUÇÃO, POR ENQUANTO O OUTRO <a href='https://www.youtube.com/watch?v=xWDf0WSWKXw&list=PL20h5VCW6xQ1oRpqimGkgzdLGTBc6rZCl&index=1&ab_channel=Raccoonzin'>SITE PARA CONTATO ESTÁ FUNCIONANDO</a></h1>
     </>
   )
 }
