524dbd9d794d235184e230590eee9b3546bfae04
Linsmar <linsmarvital@gmail.com>
2022-04-13 04:39:58 +0100

    fix metatag

--- meusite/pages/_app.js
+++ meusite/pages/_app.js
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import Head from "next/head";
 import "../styles/tailwind.css";
-import PortfolioMiniImg from "../src/assets/portfoliomini.png";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -16,32 +15,26 @@ function MyApp({ Component, pageProps }) {
         <meta property="og:title" content="Linsmar - Web Developer Portfolio" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://www.linsmarvital.com" />
-        <meta
-          property="og:image"
-          content={"https://www.linsmarvital.com" + PortfolioMiniImg.src}
-        />
+        <meta property="og:image" content="https://i.imgur.com/lwWAZTO.png" />
         <meta
           property="og:description"
           content="LINSMAR VITAL. Front-end developer and Computer Science student exploring the world of web development and its technologies and new experiences."
         />
-        <meta name="theme-color" content="#392259"></meta>
-        <meta name="twitter:card" content="summary_large_image"></meta>
+        <meta name="theme-color" content="#392259" />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta
           name="twitter:title"
           content="Linsmar - Web Developer Portfolio"
-        ></meta>
+        />
         <meta
           name="twitter:description"
           content="LINSMAR VITAL. Front-end developer and Computer Science student exploring the world of web development and its technologies and new experiences."
-        ></meta>
-        <meta
-          name="twitter:image"
-          content={"https://www.linsmarvital.com" + PortfolioMiniImg.src}
-        ></meta>
+        />
+        <meta name="twitter:image" content="https://i.imgur.com/lwWAZTO.png" />
         <meta
           name="twitter:image:alt"
           content="Linsmar Vital Picture and description about him"
-        ></meta>
+        />
       </Head>
       <Component {...pageProps} />
     </>
